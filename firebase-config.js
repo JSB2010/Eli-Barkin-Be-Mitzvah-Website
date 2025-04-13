78e94edbe2f81908dd74c88294502e80f72116ca
Jacob Barkin <Jacobsamuelbarkin@gmail.com>
2025-04-13 17:25:48 -0600

    Update Firebase configuration with project credentials and add analytics support

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBgAXectfPhAr3HqASJCewnfQJnGnfGAK8",
+  authDomain: "eli-barkin-be-mitzvah.firebaseapp.com",
+  projectId: "eli-barkin-be-mitzvah",
+  storageBucket: "eli-barkin-be-mitzvah.firebasestorage.app",
+  messagingSenderId: "1058445082947",
+  appId: "1:1058445082947:web:8ab0696d5782e63ddaeff6",
+  measurementId: "G-QQBCYHVB9C"
 };
 
 // Initialize Firebase
@@ -14,5 +15,12 @@ firebase.initializeApp(firebaseConfig);
 // Get Firestore instance
 const db = firebase.firestore();
 
+// Initialize Analytics if available
+let analytics = null;
+if (firebase.analytics) {
+  analytics = firebase.analytics();
+}
+
 // Export the db for use in other files
 window.db = db;
+window.analytics = analytics;
